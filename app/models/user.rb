bbff44e825ff38028d7d31e78144f4121bfa8ffb
yuki takagi <yuki99takagi@gmail.com>
2020-04-17 15:47:40 +0900

    コールバック設定

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  before_create CreateAgeAtCallback.new
+  before_create :ensure_has_name
+  private
+
+  def ensure_has_name
+    puts "nameの値をTaroに設定します！"
+    self.name = 'Taro' if name.blank?
+  end
+end
